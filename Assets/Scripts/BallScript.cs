e002659b0d308acace001928f177c26e1599f5c4
Sourab Bavikatte Ravi <sourabbr@gmail.com>
2023-09-19 23:35:35 -0700

    Add ball mechanics

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public BallManager ballManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballManager = FindObjectOfType<BallManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            ballManager.HandleBallCollision(gameObject);
+        }
+    }
+}
