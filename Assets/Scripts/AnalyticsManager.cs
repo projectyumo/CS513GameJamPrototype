ffebbaeea5534911f148ed53c31ec3ee499bdbdd
nthacks <nthacks@users.noreply.github.com>
2023-10-08 21:03:08 -0700

    Fix for bug #12 "A Native Collection has not been disposed, resulting in a memory leak" Cleanup UnityWebRequest correctly

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -6,16 +6,22 @@ public class AnalyticsManager : MonoBehaviour
 {
     // Unique ID for each game session
     private readonly string _gameSessionId = System.Guid.NewGuid().ToString();
+
     // Number of shots taken (total)
     public int shotsTaken = 0;
+
     // Number of shots taken (per level)
     public int shotsTakenLevel = 0;
+
     // Number of shots taken (per ball)
     public int shotsTakenBall = 0;
+
     // Number of balls knocked off (total)
     public int ballsKnockedOff = 0;
+
     // Number of bullet collisions (total)
     public int bulletCollisions = 0;
+
     // Time taken to complete game
     public float timeTakenGame = 0;
 
@@ -33,21 +39,24 @@ public class AnalyticsManager : MonoBehaviour
     IEnumerator PushDataCoroutine(string url, string jsonPayload)
     {
         // Using PUT to overwrite data at the specified location or create it if not existent
-        var request = new UnityWebRequest(url, "PUT");
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonPayload);
-        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        using (var request = new UnityWebRequest(url, "PUT"))
         {
-            Debug.LogError(request.error);
-        }
-        else
-        {
-            Debug.Log("Analytics logged successfully");
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonPayload);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError ||
+                request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                Debug.Log("Analytics logged successfully");
+            }
         }
     }
 }
