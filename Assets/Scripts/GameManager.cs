c4e05697a2146bf1b24b72fa1d3701f95e714f1c
zhzhh97 <zhuzhiheng97@outlook.com>
2023-10-10 12:18:09 -0700

    Add Bullet Count and Scoring

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int bulletCount = 2;
+    public int totalPoints = 0;
+
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI bulletCountText;
+    public TextMeshProUGUI pointText;
+
+
+    void Start()
+    {
+        bulletCountText.text = "Remaining shot: " + bulletCount.ToString();
+        pointText.text = "Score: " + totalPoints.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bulletCount == -1)
+        {
+            gameOverText.gameObject.SetActive(true);
+            gameOverText.text = "Game Over!";
+        }
+    }
+
+    public void BulletCountDown()
+    {
+        bulletCount--;
+        if (bulletCount!= -1) {
+            bulletCountText.text = "Remaining shot: " + bulletCount.ToString();
+        }
+        
+        // All balls are knocked off
+        
+    }
+
+    public void addOnePoint()
+    {
+        totalPoints += 1;
+        pointText.text = "Score: " + totalPoints.ToString();
+    }
+
+    public void addFivePoints()
+    {
+        totalPoints += 5;
+        pointText.text = "Score: " + totalPoints.ToString();
+    }
+
+    public void addTenPoints()
+    {
+        totalPoints += 10;
+        pointText.text = "Score: " + totalPoints.ToString();
+    }
+}
---
+++ Assets/Scripts/TenPoints.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenPoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameManager gameManager;
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            gameManager.addTenPoints();
+
+        }
+    }
+}
