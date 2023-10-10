6de79af04a58687eff5deb1d246c3d865fab754a
projectyumo <projectyumo@gmail.com>
2023-10-10 07:03:52 -0700

    Issue 4: Tracking, Tracing, Replaying of Bullets - Mainly wanted to instantiate ghost bullet so the player is aware that something may happen with ghost bullte. Part 2 may implement a line renderer.

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class BulletControl : MonoBehaviour
 {
     private AnalyticsManager _analyticsManager;
-    
+
     void Start()
     {
         _analyticsManager = FindObjectOfType<AnalyticsManager>();
     }
     void Update()
     {
-        if (this.GetComponent<Rigidbody2D>().velocity.magnitude < 4){
+        if (this.GetComponent<Rigidbody2D>().velocity.magnitude < 4 && this.name!="idleGhost"){
             Destroy(gameObject);
         }
     }
@@ -18,12 +18,13 @@ public class BulletControl : MonoBehaviour
     //Detect collisions between the appropriate surfaces
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("COLLISION WITH:" + collision.gameObject.name);
         //Check for a collision with Ball
         if (collision.gameObject.name.Substring(0, 4) == "Ball" || collision.gameObject.name.Substring(0, 5) == "Floor")
         {
             Destroy(gameObject);
         }
-        
+
         // TODO: Update stats if bullet collides with another bullet
         // if (collision.gameObject.CompareTag("Bullet"))
         // {
