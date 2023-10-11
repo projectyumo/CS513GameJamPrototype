975e412f2009aeab3ee74c8bdaa7713532107587
Sourab Bavikatte Ravi <sourabbavikatteravi@Sourabs-Air.lan>
2023-10-11 00:29:28 -0700

    Introduce GameManager singleton instance to control scene transitions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton instance
+    public static GameManager instance;
+
+    // On Awake, set up the Singleton pattern
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // This object will persist across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy any duplicate GameManagers
+        }
+    }
+
+    // Method to load the next scene
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            // If there are no more scenes to load, loop back to the first scene (main menu)
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Method to load a specific scene by name
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
