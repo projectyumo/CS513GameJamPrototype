2dc0f1c61fd285805a1549d5ee697143f8833fd3
Sourab Bavikatte Ravi <sourabbr@gmail.com>
2023-10-11 00:29:28 -0700

    Introduce GameManager singleton instance to control scene transitions

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,32 +1,26 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
     
     public void LoadLevel1()
     {
-        SceneManager.LoadScene("Level1");
+        gameManager.LoadSceneByName("Level1");
     }
 
     public void LoadLevel2()
     {
-        SceneManager.LoadScene("Level2");
+        gameManager.LoadSceneByName("Level2");
     }
 
     public void LoadLevel3()
     {
-        SceneManager.LoadScene("Level3");
+        gameManager.LoadSceneByName("Level3");
     }
 }
