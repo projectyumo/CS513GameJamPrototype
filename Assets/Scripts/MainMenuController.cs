a1a0301611c3373f467010890e9e98c5bd1dbcf8
Sourab Bavikatte Ravi <sourabbr@gmail.com>
2023-10-11 00:15:02 -0700

    Create MainMenu scene and levels buttons to navigate to respective levels

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
