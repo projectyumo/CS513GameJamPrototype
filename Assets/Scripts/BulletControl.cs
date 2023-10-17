2469f791460c2c2b96c42fc8be28d9d390d34ee1
projectyumo <projectyumo@gmail.com>
2023-10-16 18:25:03 -0700

    Fixing bugs with Bullet destroy mechanic

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class BulletControl : MonoBehaviour
 {
     private AnalyticsManager _analyticsManager;
-    public int destroyTime = 7;
     public int minVelocity = 4;
 
     void Start()
@@ -12,7 +11,6 @@ public class BulletControl : MonoBehaviour
     }
     void Update()
     {
-        Destroy(gameObject, destroyTime);
         if (this.GetComponent<Rigidbody2D>().velocity.magnitude < minVelocity && this.name!="idleGhost"){
             Destroy(gameObject);
         }
@@ -21,7 +19,7 @@ public class BulletControl : MonoBehaviour
     //Detect collisions between the appropriate surfaces
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("COLLISION WITH:" + collision.gameObject.name);
+        Debug.Log(this.name + "COLLISION WITH:" + collision.gameObject.name);
         //Check for a collision with Ball
         if (collision.gameObject.name.Substring(0, 4) == "Ball" || collision.gameObject.name == "Floor" || collision.gameObject.name.Substring(0, 4) == "Pock")
         {
