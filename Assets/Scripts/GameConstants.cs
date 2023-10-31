677b620d75910517c4098ca88ca03349d407edb9
nthacks <nthacks@users.noreply.github.com>
2023-10-31 16:46:46 -0700

    Update level 6 and 7 with the ghost wall mechanic and update build

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -4,7 +4,7 @@ public static class GameConstants
     static public readonly int winTextDisplayTime = 3;
     static public readonly int maxShotCount = 25; // Global max for analytics
     
-    static public readonly string winText = "You Win!";
+    static public readonly string winText = "Level Cleared!";
     static public readonly string loseText = "You Lose!";
     static public readonly string remainingShotsText = "Remaining shots: ";
     static public readonly string scoreText = "Score: ";
