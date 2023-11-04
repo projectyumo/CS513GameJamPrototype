6760e047f09e33f6ed1d665c7ad06ebb451d24a5
nthacks <nthacks@users.noreply.github.com>
2023-11-04 10:45:32 -0700

    Add analytics for glass shelf: Passthroughs and collisions

---
+++ Assets/Scripts/GlassShelfManager.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class GlassShelfManager : MonoBehaviour
+{
+    private AnalyticsManager _analyticsManager;
+
+    private void Start()
+    {
+        _analyticsManager = FindObjectOfType<AnalyticsManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("GhostBullet")) return;
+        _analyticsManager.ld.glassShelfPassthroughs++;
+        _analyticsManager.LogAnalytics();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Bullet")) return;
+        _analyticsManager.ld.glassShelfCollisions++;
+        _analyticsManager.LogAnalytics();
+    }
+}
