da915ac6b749f671f4971f555333c99aef560f39
nthacks <nthacks@users.noreply.github.com>
2023-11-05 15:45:30 -0800

    Add score icon

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -7,6 +7,5 @@ public static class GameConstants
     static public readonly string winText = "Level Cleared!";
     static public readonly string loseText = "You Lose!";
     static public readonly string remainingShotsText = "Remaining shots: ";
-    static public readonly string scoreText = "Score: ";
     static public readonly string currentLevelText = "Level: ";
 }
