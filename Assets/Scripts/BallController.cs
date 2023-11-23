47c4043ea7de7a26340012cc61312809359e7cc6
nthacks <nthacks@users.noreply.github.com>
2023-11-22 21:10:28 -0800

    Animation for ball going under the pocket

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
@@ -50,7 +52,57 @@ public class BallController : MonoBehaviour
             {
                 ballManager.BallStoppedMoving();
             }
-            Destroy(gameObject);
+            StartCoroutine(ShrinkAndDestroy(other, Mathf.Abs(_rb.velocity.x)));
         }
     }
+    private IEnumerator ShrinkAndDestroy(Collider2D other, float horizontalMoveSpeed)
+    {
+        const float moveSpeed = 8f; // Adjust this value for the moving speed
+        const float shrinkSpeed = 3f; // Adjust this value for the shrinking speed
+        const float minHorizontalSpeed = 2f; // Adjust this value for the minimum horizontal speed
+        Vector3 pocketPosition = other.gameObject.transform.position;
+        Vector3 targetScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        // Remove trigger from ball and freeze physics
+        Destroy(GetComponent<Collider2D>());
+        _rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        // Use the maximum of the horizontalMoveSpeed and minHorizontalSpeed
+        float horizontalSpeed = Mathf.Max(horizontalMoveSpeed, minHorizontalSpeed);
+    
+        // Complete the movement in stages: first horizontally, then to halfway, and finally shrinking while moving to the pocket
+        const double TOLERANCE = 0.1f;
+        while (Math.Abs(transform.position.x - pocketPosition.x) > TOLERANCE || transform.localScale.x > targetScale.x)
+        {
+            // Move to the horizontal position of the pocket
+            if (Math.Abs(transform.position.x - pocketPosition.x) > TOLERANCE)
+            {
+                transform.position = new Vector3(
+                    Mathf.MoveTowards(transform.position.x, pocketPosition.x, horizontalSpeed * Time.deltaTime),
+                    transform.position.y,
+                    transform.position.z);
+            }
+            // Once horizontal position is reached, move to halfway and shrink
+            else
+            {
+                if (Vector3.Distance(transform.position, pocketPosition) > 0.1f)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, pocketPosition, moveSpeed * Time.deltaTime);
+                }
+
+                if (transform.localScale.x > targetScale.x)
+                {
+                    transform.localScale -= new Vector3(1, 1, 1) * (shrinkSpeed * Time.deltaTime);
+                }
+            }
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+
+
+
+
 }
